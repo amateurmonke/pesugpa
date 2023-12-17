9beb8931862403f1f4c9fd35b622cd6b16776188
amateurmonke <86455454+amateurmonke@users.noreply.github.com>
2023-12-17 23:03:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
-#function to calculate grade
 def grade(marks):
     if marks == 100:
         return 10
@@ -11,7 +9,6 @@ def grade(marks):
     else:
         return 0
 
-#subject data
 def get_subject_data(semester_index, subject_index):
     name = st.text_input(f"Enter the name of subject {subject_index} for Semester {semester_index}: ")
     credits = st.number_input("Enter the credits of the subject: ", min_value=0, step=1, value=1, key=f"credits_{semester_index}_{subject_index}")
@@ -29,7 +26,6 @@ def get_subject_data(semester_index, subject_index):
         st.warning("Please enter valid marks (comma-separated)")
         return 0, 0
 
-#sgpa function
 def calculate_sgpa(subjects_data):
     sum_credits = sum(credits for credits, _ in subjects_data)
     
@@ -40,10 +36,9 @@ def calculate_sgpa(subjects_data):
         st.warning("Total credits for the semester should be greater than zero.")
         return 0
 
-#CGPA function
 def calculate_cgpa(semester_data):
     total_semesters = len(semester_data)
-    cumulative_sgpa = sum(semester_sgpa for semester_sgpa, in semester_data)
+    cumulative_sgpa = sum(semester_sgpa for semester_sgpa, _ in semester_data)
     
     if total_semesters > 0:
         cgpa = cumulative_sgpa / total_semesters
@@ -51,7 +46,6 @@ def calculate_cgpa(semester_data):
     else:
         st.warning("Number of semesters should be greater than zero.")
         return 0
-
 
 def input_semester(semester_index):
     st.subheader(f"Semester {semester_index}:")
@@ -71,18 +65,17 @@ def calculate_required_sgpa(current_cgpa, target_cgpa, completed_semesters):
         required_sgpa = (target_cgpa * (completed_semesters + 1) - current_cgpa * completed_semesters) / 1
         return required_sgpa
 
-#UI
 def main():
     st.title("GPA Calculator")
     st.write("This is a simple GPA calculator for PES University students.")
     st.write("Besides the fact that we are students of PES University, we have no other sort of formal affiliation with the management of the university. Thus, the calculation methods used in this might not reflect PESU's actual calculation methods. This is just a fun CS semester 1 mini project :D")
-    
+    # Input for the semesters
     n = st.number_input("Enter the number of semesters completed: ", min_value=1, step=1, value=1, key="num_semesters")
     semester_data = [input_semester(i) for i in range(1, int(n) + 1)]
     final_cgpa = calculate_cgpa(semester_data)
     st.success(f"Your CGPA is {final_cgpa:.2f}")
 
-    #Visualization
+    # Visualization
     df_sgpa = pd.DataFrame({
         'Semester': list(range(1, int(n) + 1)),
         'SGPA': [sgpa for sgpa, _ in semester_data]
@@ -93,16 +86,15 @@ def main():
         'CGPA': [calculate_cgpa(semester_data[:i + 1]) for i in range(int(n))]
     })
 
-    #Line charts
-
+    # Line chart for SGPA
     st.subheader("SGPA Over Semesters")
     st.line_chart(df_sgpa.set_index('Semester'))
 
-    
+    # Line chart for CGPA
     st.subheader("CGPA Over Semesters")
     st.line_chart(df_cgpa.set_index('Semester'))
 
-    #Target CGPA calculator
+    # Section for calculating required SGPA for the next semester
     st.header("Target CGPA Calculator")
     completed_semesters = st.number_input("Number of semesters completed: ", min_value=0, step=1, value=1, key="completed_semesters")
     current_cgpa = st.number_input("Current CGPA: ", min_value=0.0, value=final_cgpa, key="current_cgpa")
